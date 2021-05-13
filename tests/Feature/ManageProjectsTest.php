d825a6628cc0bd23de4ab21831fbff14bff7e049
Teddy.F <teddy.francfort@gmail.com>
2021-05-13 18:07:07 +0400

    Test guests cannot create project

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -18,6 +18,7 @@ class ManageProjectsTest extends TestCase
     {
         $project = Project::factory()->create();
         $this->get('/projects')->assertRedirect('/login');
+        $this->get('/projects/create')->assertRedirect('/login');
         $this->get($project->path())->assertRedirect('/login');
         $this->post('/projects', $project->toArray())->assertRedirect('/login');
     }
